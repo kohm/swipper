dadc3fe2c456ea27b9d2303341b4a4e1977ea6c2
lucio.poveda@gmail.com <lucio.poveda@glb-d0471.globant.com>
2014-09-18 16:23:11 -0300

    proxy

--- server/insertLocations.js
+++ server/insertLocations.js
@@ -12,7 +12,7 @@ var readJson = function(callback){
     request({
         url: url,
         json: true,
-        proxy: "http://proxy.corp.globant.com:3128"
+        //proxy: "http://proxy.corp.globant.com:3128"
     }, function (error, response, body) {
         if (!error && response.statusCode === 200) {
             callback(body);
