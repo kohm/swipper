b765a6f80c6033a336cfa7ba80fe768c133b2fa0
lucio.poveda@gmail.com <lucio.poveda@glb-d0471.globant.com>
2014-10-16 17:40:23 -0300

    asd

--- common/models/config.js
+++ common/models/config.js
@@ -1,4 +1,4 @@
-//exports.apiKey = "AIzaSyDrbdn_7ZinfRiLaHsYPG0VpNlH7u7udp4"; //server
-exports.apiKey = "AIzaSyAyeLAbHzmMtrjOO_yVwGYs4Xg7iYbpVdM"; //browser
+exports.apiKey = "AIzaSyDT_7HU59iNKx1zEQDj2wbCGP65BkoEXqs"; //Aux
+//exports.apiKey = "AIzaSyAyeLAbHzmMtrjOO_yVwGYs4Xg7iYbpVdM"; //browser
 exports.outputFormat = "json";
 console.log("asd");
